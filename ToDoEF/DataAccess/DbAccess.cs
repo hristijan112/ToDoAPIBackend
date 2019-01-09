88d5b2766d646e645669abb9b03f24e8714f9815
Hristijan <Hristijan@DESKTOP-J67T9JJ>
2019-01-09 16:06:45 +0100

    Created Contracts and Database Access. Next is writing CRUD in DataRepository.

---
+++ ToDoEF/DataAccess/DbAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoEF.DataAccess;
+
+namespace ToDoEF.DataAccess
+{
+    public class DbAccess: IDbAccess
+    {
+        public todoListEntities GetContext()
+        {
+            return new todoListEntities();
+        }
+    }
+}
+
